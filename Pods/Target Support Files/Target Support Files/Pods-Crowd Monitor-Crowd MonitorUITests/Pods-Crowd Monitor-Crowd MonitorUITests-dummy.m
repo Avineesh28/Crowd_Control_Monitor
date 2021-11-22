aa56bbafcab879671fac05413d3d257a137e6bb2
Avineesh Sathyakumar <avineeshsathyakumar@gmail.com>
2021-11-22 21:57:31 +0530

    Delete Pods/Target Support Files directory

--- Pods/Target Support Files/Target Support Files/Pods-Crowd Monitor-Crowd MonitorUITests/Pods-Crowd Monitor-Crowd MonitorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Crowd_Monitor_Crowd_MonitorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Crowd_Monitor_Crowd_MonitorUITests
-@end
--- Pods/Target Support Files/Target Support Files/Pods-Crowd Monitor/Pods-Crowd Monitor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Crowd_Monitor : NSObject
-@end
-@implementation PodsDummy_Pods_Crowd_Monitor
-@end
